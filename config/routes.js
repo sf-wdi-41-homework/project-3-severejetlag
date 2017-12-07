514b474f6d2718f991247241b023cbb3aa9dbeba
severejetlag <nhl1013@gmail.com>
2017-12-07 14:34:56 -0800

    Created repo route to allow users to view a repo that they own

--- config/routes.js
+++ config/routes.js
@@ -8,19 +8,26 @@ require("./passport")(passport)
 router.route('/')
   .get(controllers.statics.home);
 
+// Route post login
 router.route('/profile')
   .get(controllers.users.profile)
 
+router.route('/repo/:name')
+  .get(controllers.users.repo)
+
+// GitHub OAuth route
 router.route('/auth/github')
   .get(passport.authenticate('github'))
 
+// Route after GitHub OAuth
 router.route('/auth/github/callback')
   .get(passport.authenticate('github', { failureRedirect: '/' }),
   function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/');
+    // Successful authentication, redirect profile.
+    res.redirect('/profile');
   });
 
+// Route to de-auth user
 app.get("/logout", function(req, res){
     req.logout();
     res.redirect("/")
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,18 +1,22 @@
 const db = require('../models');
 const request = require('request');
 
+// Loads profile page after login
 let profile = (req,res) => {
+  // Get Requst using Users repo list url and access token
   request.get({
-    url: req.user.gitHub.reposUrl,
+    url: 'https://api.github.com/user/repos',
     headers:{
       'User-Agent': "Github Dashboard App",
       'Authorization': `token ${req.user.gitHub.accessToken}`
     }
     }, function(err, response, body) {
+    // If successful then render profile page
     if (!err && response.statusCode == 200) {
+      console.log(JSON.parse(body))
       var repos = JSON.parse(body);
       res.render(
-        'index.ejs',
+        'profile.ejs',
         {
           message: req.flash('errorMessage'),
           user:req.user,
@@ -23,9 +27,36 @@ let profile = (req,res) => {
   })
   // request.get('https://api.github.com/rate_limit', {headers:{'User-Agent': "Github Dashboard App",'Authorization': `token ${req.user.gitHub.accessToken}`}}, function(err, response, body) {
   //   console.log(JSON.parse(body))
+  //   res.send('ok')
   // })
 }
 
+let repo = (req,res) => {
+  repo = req.params.name
+  request.get({
+    url: `https://api.github.com/repos/${req.user.gitHub.username}/${repo}`,
+    headers:{
+      'User-Agent': "Github Dashboard App",
+      'Authorization': `token ${req.user.gitHub.accessToken}`
+    }
+    }, function(err, response, body) {
+    // If successful then render profile page
+    if (!err && response.statusCode == 200) {
+      console.log(JSON.parse(body))
+      let repoInfo = JSON.parse(body);
+      res.render(
+        'repo.ejs',
+        {
+          message: req.flash('errorMessage'),
+          user:req.user,
+          repoInfo
+        }
+      )
+    }
+  })
+}
+
 module.exports = {
-  profile
+  profile,
+  repo
 }
