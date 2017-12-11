8df56067aca1f1727145c539e545d59379263796
severejetlag <nhl1013@gmail.com>
2017-12-11 11:55:41 -0800

    Implementing api requests to grab repo status

--- controllers/reposController.js
+++ controllers/reposController.js
@@ -2,22 +2,41 @@ const db = require('../models');
 const request = require('request');
 
 let show = (req,res) => {
-  repo = req.params.name
+  repo = `https://api.github.com/repos/${req.user.gitHub.username}/${req.params.name}`
   request.get({
-    url: `https://api.github.com/repos/${req.user.gitHub.username}/${repo}`,
+    url: repo,
     headers:{'User-Agent': "Github Dashboard App",'Authorization': `token ${req.user.gitHub.accessToken}`}
     }, function(err, response, body) {
     // If successful then render profile page
     if (!err && response.statusCode == 200) {
       let repoInfo = JSON.parse(body);
-      res.render(
-        'repo.ejs',
-        {
-          message: req.flash('errorMessage'),
-          user:req.user,
-          repoInfo
+      console.log(repoInfo)
+      request.get({
+        url:repoInfo.languages_url,
+        headers:{'User-Agent': "Github Dashboard App",'Authorization': `token ${req.user.gitHub.accessToken}`}
+      }, function(err, response, body){
+        if(!err && response.statusCode === 200){
+          let repoLanguages = JSON.parse(body);
+          request.get({
+            url: `${repo}/stats/contributors`,
+            headers:{'User-Agent': "Github Dashboard App",'Authorization': `token ${req.user.gitHub.accessToken}`}
+          },function(err,response,body){
+            if(!err && response.statusCode === 200){
+              let repoContributors = JSON.parse(body)
+              console.log(repoContributors)
+              res.render(
+                'repo.ejs',
+                {
+                  message: req.flash('errorMessage'),
+                  user:req.user,
+                  repoInfo,
+                  repoLanguages
+                }
+              )
+            }
+          })
         }
-      )
+      })
     }
   })
 }
